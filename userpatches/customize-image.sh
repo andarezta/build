4a184abece83136f0a3b47d5fd9e1eeee4878b69
andarezta <96639750+andarezta@users.noreply.github.com>
2024-06-26 17:45:55 +0700

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -1,1 +1,2 @@
+sudo apt-get update
 sudo apt-get install uuid-dev git wget cmake curl build-essential libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev libboost-all-dev libomp-dev libjansson-dev libcurl4-openssl-dev automake autotools-dev libsodium-dev clang nodejs npm
