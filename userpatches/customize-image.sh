72987351b7851b339366a8c0f8c6f8c5437fd44d
andarezta <96639750+andarezta@users.noreply.github.com>
2024-06-26 17:26:21 +0700

    Add files via upload

---
+++ userpatches/customize-image.sh
@@ -0,0 +1,1 @@
+sudo apt-get install uuid-dev git wget cmake curl build-essential libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev libboost-all-dev libomp-dev libjansson-dev libcurl4-openssl-dev automake autotools-dev libsodium-dev clang nodejs npm
